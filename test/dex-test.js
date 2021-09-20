3fd4a0f218018b0399137c658b47d5e8d2baea88
jjthug <jossychemmazham@gmail.com>
2021-09-21 01:02:06 +0530

    complete wallet tests

--- test/dex-test.js
+++ test/dex-test.js
@@ -4,24 +4,23 @@ const { assert } = require('chai');
 const truffleAssert = require('truffle-assertions');
 
 describe("Dex", (accounts) => {
-
+let dex, aave;
     before(async function () {
         accounts = await web3.eth.getAccounts();
+        dex = await Dex.new();
+        aave = await Aave.new();
       });
 
     it("should only be possible for owner to add tokens", async() => {
-        const dex = await Dex.new();
-        const aave = await Aave.new();
 
         truffleAssert.passes(
             await dex.addToken(web3.utils.utf8ToHex("Aave"), aave.address,{from: accounts[0]}));
-        // truffleAssert.reverts(
-        //     await dex.addToken(web3.utils.utf8ToHex("POLKA"), aave.address,{from: addr1.address}));
+            await truffleAssert.reverts(
+             dex.addToken(web3.utils.utf8ToHex("POLKA"), aave.address,{from: accounts[1]}));
+
     })
 
     it("should handle deposits correctly", async() => {
-        const dex = await Dex.new();
-        const aave = await Aave.new();
         //[owner,addr1,addr2] = await ethers.getSigners(); can use using ethers, owner.address, addr1.address, ...
 
         await dex.addToken(web3.utils.utf8ToHex("Aave"), aave.address,{from: accounts[0]});
@@ -29,8 +28,19 @@ describe("Dex", (accounts) => {
         await dex.deposit(web3.utils.utf8ToHex("Aave"),100); // now dex can deposit
         let balance = await dex.balances(accounts[0], web3.utils.utf8ToHex("Aave"));
         assert.equal(balance, 100);
+    })
 
+    it("should handle faulty withdrawals correctly", async() => {
 
+        await dex.addToken(web3.utils.utf8ToHex("Aave"), aave.address,{from: accounts[0]});
+        await truffleAssert.reverts(dex.withdraw(web3.utils.utf8ToHex("Aave"),6000),"Insufficient balance");
+
+    })
+
+    it("should handle correct withdrawals correctly", async() => {
+
+        await dex.addToken(web3.utils.utf8ToHex("Aave"), aave.address,{from: accounts[0]});
+        await truffleAssert.passes(dex.withdraw(web3.utils.utf8ToHex("Aave"),100));
 
     })
 })
